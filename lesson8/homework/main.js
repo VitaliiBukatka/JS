9df78af1be6fd084edae8d47e6bc90bfb72422dc
VitaliiBukatka <vvbukatka@gmail.com>
2022-02-20 20:27:47 +0200

    done

---
+++ lesson8/homework/main.js
@@ -0,0 +1,48 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+//
+//     - Напишіть код,  котрий :
+// -- отримує текст з параграфа з id "content"
+
+// let txtContById = document.getElementById('content');
+// console.log(txtContById);
+
+// -- отримує текст з блоку з id "rules"
+
+// let txtRulById = document.getElementById('rules');
+// console.log(txtRulById);
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+
+// let txtNewContById = document.getElementById('content');
+// txtNewContById.innerText = 'qwerty';
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+// let txtNewRulById = document.getElementById('rules');
+// txtNewRulById.innerText = 'asd';
+
+// -- змініть кожному елементу колір фону на червоний
+
+// let allElBCol = document.body.children;
+// for (const allElElement of allElBCol) {
+//     allElElement.style.backgroundColor = 'red';
+// }
+
+// -- змініть кожному елементу колір тексту на синій
+
+// let allElTxtCol = document.body.children;
+// for (const allElTxtColElement of allElTxtCol) {
+//     allElTxtColElement.style.color = 'blue';
+// }
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// let allClass = document.getElementById('rules');
+// console.log(allClass.classList);
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let txtRedRul = document.getElementsByClassName('fc_rules');
+// for (const txtRedRulElement of txtRedRul) {
+//     txtRedRulElement.style.color = 'red';
+// }
